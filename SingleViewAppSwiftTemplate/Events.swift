53207f7d3833db0c863fa1138b7234f837aa2a72
Marco Grier <mlgrier@gmail.com>
2018-12-30 07:21:41 -0600

    Added Struct For Events

---
+++ SingleViewAppSwiftTemplate/Events.swift
@@ -0,0 +1,54 @@
+//
+//  Events.swift
+//  SingleViewAppSwiftTemplate
+//
+//  Created by Marco Grier on 12/30/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct historicalEvents {
+    let events: [[String : String]] = [
+        ["Event": "What is 0 + 1", "Year": "1"],
+        ["Event": "What is 1 + 1", "Year": "2"],
+        ["Event": "What is 1 + 2", "Year": "3"],
+        ["Event": "What is 1 + 3", "Year": "4"],
+        ["Event": "What is 9 - 8", "Year": "1"],
+        ["Event": "What is 12 - 10", "Year": "2"],
+        ["Event": "What is 13 - 10", "Year": "3"],
+        ["Event": "What is 14 - 10", "Year": "4"],
+        ["Event": "What is 7 - 6", "Year": "1"],
+        ["Event": "What is 22 - 20", "Year": "2"],
+        ["Event": "What is 33 - 30", "Year": "3"],
+        ["Event": "What is 100 - 96", "Year": "4"],
+        ["Event": "What is 29 - 28", "Year": "1"],
+        ["Event": "What is 8 - 6", "Year": "2"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 17 - 14", "Year": "3"],
+        ["Event": "What is 48 - 44", "Year": "4"]
+    ]
+}
